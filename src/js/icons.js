9a9d12d1f68410a0890affce038cdb945b5f4b38
kry008 <pkrzysaaaa@gmail.com>
2021-03-20 09:10:04 +0100

    Add files via upload

---
+++ src/js/icons.js
@@ -0,0 +1,62 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import {
+    faUndo,
+    faTrash,
+    faCheck,
+    faRedo,
+    faMousePointer,
+    faPencilAlt,
+    faEraser,
+    faImage,
+    faFont,
+    faSave,
+    faUpload,
+    faShareSquare,
+    faAngleLeft,
+    faAngleRight,
+    faSortDown,
+    faExpandArrowsAlt,
+    faLock,
+    faLockOpen,
+    faInfoCircle,
+    faGlobe,
+    faStickyNote,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+    faSquare,
+    faCircle,
+    faFile,
+    faFileAlt,
+    faPlusSquare,
+} from "@fortawesome/free-regular-svg-icons";
+
+library.add(
+    faUndo,
+    faTrash,
+    faCheck,
+    faRedo,
+    faMousePointer,
+    faPencilAlt,
+    faEraser,
+    faImage,
+    faFont,
+    faSave,
+    faUpload,
+    faShareSquare,
+    faAngleLeft,
+    faAngleRight,
+    faSortDown,
+    faExpandArrowsAlt,
+    faSquare,
+    faCircle,
+    faFile,
+    faFileAlt,
+    faPlusSquare,
+    faLock,
+    faLockOpen,
+    faInfoCircle,
+    faGlobe,
+    faStickyNote
+);
+
+dom.i2svg();
